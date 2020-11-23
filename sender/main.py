dda12df2ea45edae99b6b36d4dec2f6c465b8e45
Tymur Riazantsev <triaznt@cisco.com>
2020-11-23 18:00:46 +0200

    ...workflow...

--- sender/main.py
+++ sender/main.py
@@ -8,18 +8,19 @@ import time
 def send_post(file):
     import requests
     url = 'http://0.0.0.0:5000/post-example'
-    data_text = open("./data.txt", "r").read()
-    data_json = {'data': data_text}
+    data_json = open(file, "r").read()
+    # data_json = {'data': data_text}
     x = requests.post(url, json=data_json)
     print(data_json)
     print(x.text)
-list = [
-    "./sender/1.json",
-    "./sender/2.json",
-    "./sender/3.json"
-]
 
+list = [
+    "./1.json",
+    "./2.json",
+    "./3.json"
+]
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     for i in list:
         print(i)
+        send_post(i)
